eeeea5424fae472202c6b661144483a187019b1b
tachung <tachung.chi@databricks.com>
2024-10-12 11:15:16 -0400

    change model and dataset

--- compute_block_similarity/run_layer_similarity.sh
+++ compute_block_similarity/run_layer_similarity.sh
@@ -3,11 +3,11 @@
 # This Bash script runs the Python script with arguments
 
 # Run the Python script with command-line arguments
-python layer_similarity.py --model_path "mistralai/Mistral-7B-Instruct-v0.2" \
-                      --dataset "arcee-ai/sec-data-mini" \
+python layer_similarity.py --model_path ./meta-llama/Meta-Llama-3.1-8B \
+                      --dataset stas/c4-en-10k \
                       --dataset_column "text" \
                       --batch_size 8 \
                       --max_length 1024 \
                       --layers_to_skip 8 \
-                      --dataset_size 4000 \
+                      --dataset_size 1000 \
                       --dataset_subset "train" 
